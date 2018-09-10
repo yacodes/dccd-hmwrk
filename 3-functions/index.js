88dedf5b9718ef058efc014afdb274214e3ce8f5
Aleksandr Yakunichev <canvaskisa@gmail.com>
2018-09-10 17:12:12 +0300

    Add homework for functions lesson

---
+++ 3-functions/index.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const fs = require('fs');
+
+const pathToJSON = path.resolve(__dirname, 'index.json');
+
+// your code...
+// console.log(count);
+// console.log(averageAge);
+// fs.writeFileSync('users.csv', 'data', 'utf8');
