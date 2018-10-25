c8c597b78ffadd4fbdac832b5250338743f2e820
Aleksandr Yakunichev <canvaskisa@gmail.com>
2018-10-25 14:26:10 +0300

    Add homework for day 14 module bundlers

---
+++ 14-module-bundlers/src/index.js
@@ -0,0 +1,15 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+require('./index.css');
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
