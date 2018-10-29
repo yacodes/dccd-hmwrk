41a30c59ead9de524901dd4e4bea96dc71d7a918
Aleksandr Yakunichev <canvaskisa@gmail.com>
2018-10-29 19:59:07 +0300

    Add homework for day 15

---
+++ 15-react-1/src/App.js
@@ -0,0 +1,9 @@
+const React = require('react');
+
+const App = () => (
+  <main>
+    App
+  </main>
+);
+
+export default App;
---
+++ 15-react-1/src/index.js
@@ -0,0 +1,6 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+require('./index.css');
+const App = require('./App');
+
+ReactDOM.render(<App/>, document.getElementById('root'));
